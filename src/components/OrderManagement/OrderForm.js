400568389d878ca08737efd6448d07e68099a368
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-09-25 12:47:20 +0530

    Delete src directory

--- src/components/OrderManagement/OrderForm.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const OrderForm = () => {
-    return (
-        <div></div>
-    );
-};
-
-export default OrderForm;
--- src/contexts/UserContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const UserProvider = () => {
-    return (
-        <div></div>
-    );
-};
